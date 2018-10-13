21078483d43a7c0c2a66714d5d849a6ef3212f89
Juan Dominguez <j5doming@ucsd.edu>
2018-10-13 14:07:27 -0700

    Finished question and answer TextViews and set listener to question view

--- app/src/main/java/com/example/juan/flashcard/MainActivity.java
+++ app/src/main/java/com/example/juan/flashcard/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.juan.flashcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
